1a4537b7ab1613a8d12f15ffac3089b541c62ee7
Anmol Garg <garganmol11@gmail.com>
2022-09-22 10:24:18 +0530

    added project stub

---
+++ streaming_playlist/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from streaming_playlist.users.models import User
+from streaming_playlist.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ streaming_playlist/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from streaming_playlist.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
