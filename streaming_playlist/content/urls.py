41341cc9c639b3fd2130650bb498d536829fb811
Anmol Garg <garganmol11@gmail.com>
2022-11-20 19:11:19 +0530

    added series, season and episode pages

--- streaming_playlist/content/urls.py
+++ streaming_playlist/content/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 
-from streaming_playlist.content.views import MovieView, TVView, TVEpisodeView
+from streaming_playlist.content.views import (
+    MovieView,
+    TVView,
+    TVSeasonView,
+    TVEpisodeView,
+)
 
 app_name = "content"
 urlpatterns = [
     path("movie/<str:content_id>/", view=MovieView.as_view(), name="get_movie"),
     path("tv/<str:content_id>/", view=TVView.as_view(), name="get_tv"),
-    path("episode/<str:content_id>/", view=TVEpisodeView.as_view(), name="get_episode"),
+    path(
+        "tv/<str:tv_id>/season/<season_number>/",
+        view=TVSeasonView.as_view(),
+        name="get_season",
+    ),
+    path(
+        "tv/<str:tv_id>/season/<season_number>/episode/<str:episode_number>/",
+        view=TVEpisodeView.as_view(),
+        name="get_episode",
+    ),
 ]
